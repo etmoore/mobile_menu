3c5ce22d55c92b206afd37ab393f85d62721e159
Evan Moore <etmoore@gmail.com>
2014-05-16 12:52:01 -0600

    created plan of attack

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//Problem: It looks bad in smaller browser widths and small devices
+//Solution: Hide the text links and sway them out with a more appropriate navigation
+
+//Create a select and append to menu
+//Cycle over menu links
+  //create an option
+  //option's value is the href
+  //option's text is the text of links
+  //append option to select
+//Create button
+//Bind click to button
+  //Go to select's location
+//Modify CSS to hide links on small width and show button and select
+  //Also hides select and button on larger width and show links
+//Deal with selected options depending on current page
